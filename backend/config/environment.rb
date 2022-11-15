3fd44495876eac854f5ef95733e4b08205f752c3
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2022-11-15 11:31:00 +0300

    configure production and deployment

--- backend/config/environment.rb
+++ backend/config/environment.rb
@@ -4,3 +4,17 @@ require 'bundler/setup'
 Bundler.require(:default, ENV['RACK_ENV'])
 
 require_all 'app'
+
+configure :production, :development do
+    db = URI.parse(ENV['RACK_ENV'] ||
+    'sqlite3://localhost/db/development.sqlite3'
+    )
+    ActiveRecord::Base.establish_connection(
+        adapter: 'sqlite3',
+        host: db.host,
+        username: db.user,
+        password: db.password,
+        database: db.path[1..-1],
+        encoding: 'utf8'
+    )
+end
